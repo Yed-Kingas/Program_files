0ffe7c636b4950353c20883db0336293f147baaf
Rohan2005Saha <rohansaha.sssdumdum@gmail.com>
2024-08-27 15:28:35 +0530

    Dict_g.py modified by RS

--- dict_g.py
+++ dict_g.py
@@ -1,29 +1,55 @@
+# # Define the dictionary for prices
+# prices = {
+#     "apple": 2,
+#     "banana": 1,
+#     "orange": 1,
+#     "milk": 1,
+#     "bread": 1
+# }
+#
+# quantities = {
+#     "apple": 1,
+#     "banana": 1,
+#     "orange": 1,
+#     "milk": 1,
+#     "bread": 1
+# }
+#
+# total_bill = 0.0
+#
+# for item, quantity in quantities.items():
+#     total_bill += prices[item] * quantity
+#
+# print("Total bill:", total_bill)
+
+
+
 # Define the dictionary for prices
 prices = {
-    "apple": 1,
-    "banana": 1,
+    "apple": 2,
+    "banana": 2,
     "orange": 1,
     "milk": 1,
     "bread": 1
 }
-
 
 quantities = {
-    "apple": 2,
-    "banana": 1,
+    "apple": 1,
+    "banana": 2,
     "orange": 1,
-    "milk": 1,
+    "milk": 2,
     "bread": 1
 }
 
-
 total_bill = 0.0
-
+bill_details = []
 
 for item, quantity in quantities.items():
-   
-    total_bill += prices[item] * quantity
-   
+    item_cost = prices[item] * quantity
+    total_bill += item_cost
+    bill_details.append(f"{item.capitalize()}: {quantity} * {prices[item]} = {item_cost}")
 
-
-print("Total bill:", total_bill)
+bill_details.append(f"\nTotal bill: ₹ {total_bill}")
+print("\n".join(bill_details))
+print(dir(dict))
+# print(type(dict.join))
