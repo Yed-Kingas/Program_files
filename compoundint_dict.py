4d22c166090b57dd61fc2c75ebb3f28ee3bd63bc
Sagnik Dey <sagnikdey@Sagniks-MacBook-Air-2.local>
2024-09-02 11:02:29 +0530

    Modified dict_g and compound

--- compoundint_dict.py
+++ compoundint_dict.py
@@ -1,4 +1,4 @@
-def cal_si(p,n,r,q):
+def cal_ci(p,n,r,q):
     
     a=p*(1+r/q)**(n*q)
     ci=a-p
@@ -8,5 +8,5 @@ p=int(input("Enter the Principal amount :"))
 n=int(input("Enter the no of yrs :"))
 r=int(input("Enter the rate of interest :"))
 q=int(input("Enter the no of times the interest is compounded per yr :"))
-i=cal_si(p, n, r, q)
+i=cal_ci(p, n, r, q)
 print("The compound interest is", round(i,2))
